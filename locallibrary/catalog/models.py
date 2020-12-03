4a455b3191202b92e936a6ba538618b25928423b
Nikolay <nikolain073@gmail.com>
2020-12-03 22:32:29 +0200

    Update Book model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -29,9 +29,6 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     
-    class Meta:
-        verbose_name = _("Book")
-        verbose_name_plural = _("Books")
 
     def __str__(self):
         """String for representing the Model object."""
