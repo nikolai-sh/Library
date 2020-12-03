811d5f828ca563c91eed353b5990b1766a8c79c0
Nikolay <nikolain073@gmail.com>
2020-12-03 22:31:41 +0200

    Add Book model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
 # Create your models here.
 class Genre(models.Model):
@@ -10,4 +11,34 @@ class Genre(models.Model):
   
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+
+    """Model representing a book (but not a specific copy of a book)."""
+
+    title = models.CharField(max_length=200)
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in the file
+    author = models.ForeignKey('Author', on_delete=SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13, unique=True, 
+                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    
+    class Meta:
+        verbose_name = _("Book")
+        verbose_name_plural = _("Books")
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
 
