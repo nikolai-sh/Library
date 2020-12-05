6a0f742c49bb8163f01da8d3438d07501eb86509
Nikolay <nikolain073@gmail.com>
2020-12-05 18:05:05 +0200

    Add catalog.urls to localLibrary/urls

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
 ]
 
 #Add URL maps to redirect the base URL to our application
