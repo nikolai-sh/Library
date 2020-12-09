cf6ffbe9053ce7381affeb9c809d79f5dfeab5c4
Nikolay <nikolain073@gmail.com>
2020-12-09 20:12:44 +0200

    Add session  counter of main page visit

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -17,6 +17,12 @@ def index(request):
 
     #  The 'all()' is implied by default 
     num_authors = Author.objects.all().count()
+
+     # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+
     # Create list of pairs ['Genre', 'Books']
     # num_genre_books = ([genre, books] for Book.objects.filter(genre__exact='genre').count()
     genre_list = Genre.objects.all()
@@ -33,8 +39,13 @@ def index(request):
     }
 
      # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
-
+    return render(
+        request,
+        'index.html', 
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,
+                'num_authors':num_authors, 'genre_books': genre_books,
+                'num_visits':num_visits}, # num_visits appended
+    )
 # from django.views import generic
 
 class BookListView(generic.ListView):
