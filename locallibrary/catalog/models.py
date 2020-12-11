bec2cf080d915f2d29a79ab4e1b5263f6cadc41d
Nikolay <nikolain073@gmail.com>
2020-12-11 19:37:21 +0200

    Add permissios to Meta class of BookInstance class

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -79,6 +79,8 @@ class BookInstance(models.Model):
     class Meta:
         ordering = ['due_back']
 
+        permissions = (("can_mark_returned", "Set book as returned"),)   
+
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
