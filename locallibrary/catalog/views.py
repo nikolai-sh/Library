56cdab1395f0b95d737a61a3c009445f48238e60
Nikolay <nikolain073@gmail.com>
2020-12-16 13:14:49 +0200

    update AutorCreate

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -156,7 +156,8 @@ class AuthorCreate(CreateView):
     model = Author
     fields = '__all__'
     initial = {'date_of_death': '12/10/2016',}   
-
+    permission_required = 'catalog.can_mark_returned'
+    
 class AuthorUpdate(UpdateView):
     model = Author
     fields = ['first_name','last_name','date_of_birth','date_of_death']
