08da55eae460a781e0eeb01284a05c6ee0e6e060
Nikolay <nikolain073@gmail.com>
2020-12-17 17:32:02 +0200

    Bugfix LoanedBooksByUserListView

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -93,7 +93,7 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
     paginate_by = 10
     
     def get_queryset(self):
-        return BookInstance.objects.filter(borrowed=self.request.user).filter(status__exact='o').order_by('due_back')
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
 
